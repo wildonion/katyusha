4afc7345b76bca7c802e9ce74986dfd69817ee37
2017-A-15 <marefimoghaddam@unixerr.com>
2021-02-06 21:12:35 +0330

    add some socket server codes

--- katrust/src/main.rs
+++ katrust/src/main.rs
@@ -1,9 +1,57 @@
 
 
 
-// https://doc.rust-lang.org/cargo/guide/creating-a-new-project.html
 // https://docs.rs/telebot/0.3.1/telebot/
 
-fn main() {
-    println!("Hello, world!");
+
+// ==================================
+// a simple socket server using tokio
+// ==================================
+
+use std::str;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+	let addr = "0.0.0.0:8990".parse::<SocketAddr>().unwrap();
+	let listener = TcpListener::bind(&addr).await.unwrap();
+	println!("\n[+] Listening on: {}", addr);
+    let sample_string = String::from("wildonion");
+    let bytes = sample_string.as_bytes(); // turn a string into buffer (asccii)
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        println!("[+] Socket info: {:#?}", socket);
+
+        tokio::spawn(async move { // move the socket to the async and spawned scope
+            let mut buf = vec![0; 6400]; // allocating the buffer
+            loop {
+                let n = match socket.read(&mut buf).await {              
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("[!] Failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+				let data = match str::from_utf8(&buf[0..n]) {
+				        Ok(v) => v,
+				        Err(e) => panic!("[!] Invalid UTF-8 sequence: {}", e),
+				    };
+				println!("[+] Request: {}", data);
+
+				    
+                if let Err(e) = socket.write_all(b"\n[+] Response: OK\n").await {
+                    eprintln!("[!] Failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            } // loop end
+        }); // tokio spawn end
+    } // loop end
 }
+
+
