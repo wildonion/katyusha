8a5782a93c8fee43f807abab945f5874043caa0d
2017-A-15 <marefimoghaddam@unixerr.com>
2021-08-15 16:55:20 +0430

    Update utils.rs

--- katrust/src/utils.rs
+++ katrust/src/utils.rs
@@ -4,6 +4,9 @@
 
 
 /*
+
+
+
         
     https://github.com/wildonion/aravl/blob/master/docs/rust.rules
     https://gist.github.com/wildonion/4f1956d9908e348a74b4381458e474e1
@@ -56,7 +59,7 @@
 
 
 
-    TODO - custom derive macro or proc_macro for my own traits using trait scope orphan rule, closures, lifetimes, C or raw pointers like *mut and *const
+    TODO - declarative macro or proc_macro for my own traits using trait scope orphan rule, closures, lifetimes, C or raw pointers like *mut and *const
     TODO - smart pointers like Arc, Rc, RefCell, Mutex, RwLock, Pin and Box
     TODO - handle each command of bot inside tokio::spawn() as an async task   
     TODO - bot macros and functions 
